fcebb410f9cc0908b01cb60c0bf09ac3c3a090d4
Jonathan Strickland <djanatyn@gmail.com>
2021-03-13 09:29:29 -0500

    install nix-ld
    https://github.com/Mic92/nix-ld

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:djanatyn/nixpkgs";
+    nix-ld.url = "github:Mic92/nix-ld";
+    nix-ld.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   description = "nix configuration for djanatyn";
 
-  outputs = { self, nixpkgs }@inputs: {
+  outputs = { self, nix-ld, nixpkgs }@inputs: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./desktop
+          nix-ld.nixosModules.nix-ld
         ];
       };
     };
