2edac38ef560acff6bda98042c4063d9d4a6cb1d
Jonathan Strickland <djanatyn@gmail.com>
2022-08-15 09:35:02 -0400

    search-shell-history: get installed packages

--- search-shell-history.lisp
+++ search-shell-history.lisp
@@ -1,5 +1,5 @@
 (defpackage :search-history
-  (:use :cl :iterate :cl-ppcre))
+  (:use :cl :iterate :cl-ppcre :inferior-shell))
 (in-package :search-history)
 
 (defun search-package (line)
@@ -16,5 +16,10 @@
 (defun search-history (path)
   (let ((contents (str:from-file path :external-format '(:utf-8 :replacement "?"))))
     (remove-duplicates (search-lines (str:lines contents)) :test #'equal)))
+
+(defun get-installed-packages ()
+  (let* ((flake "github:djanatyn/flake")
+         (packages (format nil "~a#nixosConfigurations.desktop.config.environment.systemPackages" flake)))
+    (run/s `(nix eval --impure --raw ,packages --apply "builtins.toString"))))
 
 (search-history "~/.zsh_history")
