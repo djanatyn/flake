fb1a0a5fc0158f35fe1d1bb16a036afc8e61ca57
Jonathan Strickland <jonathan@osohq.com>
2023-02-13 09:43:25 -0500

    work: remove grafana and prometheus configs

--- work/default.nix
+++ work/default.nix
@@ -97,27 +97,6 @@
     virtualbox.host.enable = false;
   };
 
-  # enable grafana with default settings
-  services.grafana.enable = true;
-
-  # enable prometheus with node exporter
-  services.prometheus.enable = true;
-  services.prometheus.exporters.node.enable = true;
-
-  # scrape node exporter
-  services.prometheus.scrapeConfigs = [
-    {
-      job_name = "node_scraper";
-      static_configs = [{
-        targets = [
-          "${
-            toString config.services.prometheus.exporters.node.listenAddress
-          }:${toString config.services.prometheus.exporters.node.port}"
-        ];
-      }];
-    }
-  ];
-
   services = {
     xserver = {
       enable = true;
